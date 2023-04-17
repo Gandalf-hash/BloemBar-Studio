21e2cdd0309ef42121ba0a1d1794a49c65a31408
Gandalf-hash <phiritshepiso99@gmail.com>
2023-04-17 11:09:22 +0200

    All Files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import { dark } from "./styles/Theme";
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+import { useRef } from "react";
+import "locomotive-scroll/dist/locomotive-scroll.css";
+
+import Home from "./section/Home";
+import { AnimatePresence } from "framer-motion";
+import About from "./section/About";
+import Shop from "./section/Shop";
+import ScrollTriggerProxy from "./components/ScrollTriggerProxy";
+import Banner from "./section/Banner";
+import NewArrival from "./section/NewArrival";
+import Footer from "./section/Footer";
+import Loader from "./components/Loader";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const containerRef = useRef(null);
+
+const [loaded, setLoaded] = useState();
+
+useEffect(() => {
+  setTimeout(() => {
+    setLoaded(true);
+  }, 3000);
+}, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <GlobalStyles />
+
+      <ThemeProvider theme={dark}>
+        <LocomotiveScrollProvider
+          options={{
+            smooth: true,
+            // ... all available Locomotive Scroll instance options
+            smartphone:{
+              smooth: true
+            },
+            tablet:{
+              smooth: true
+            }
+          }}
+          watch={
+            [
+              //..all the dependencies you want to watch to update the scroll.
+              //  Basically, you would want to watch page/location changes
+              //  For example, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+            ]
+          }
+          containerRef={containerRef}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <AnimatePresence>
+          {loaded ? null : <Loader />}
+          </AnimatePresence>
+          <ScrollTriggerProxy />
+         <AnimatePresence>
+         <main className="App" data-scroll-container ref={containerRef}>
+            <Home />
+            <About />
+            <Shop />
+            <Banner />
+            <NewArrival />
+            <Footer />
+          </main>
+         </AnimatePresence>
+        </LocomotiveScrollProvider>
+      </ThemeProvider>
+    </>
   );
 }
 
---
+++ src/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  resolve: {
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+    },
+  },
+};
